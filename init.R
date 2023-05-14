bf089fc29494652dc4026d516d02f8a9fdfefcf6
keiranmaskell <kmaskell428@gmail.com>
2023-05-14 12:34:25 -0400

    updated init file to automate package installation and loading
    added resources directory
    
    specification of default/non-default working directory filepath
    
    comments explaining the init.R script

--- init.R
+++ init.R
@@ -1,30 +1,56 @@
 
+#default_wd <- "~/Dropbox/tripartite-symbiosis/new_code"
+default_wd <- "/Users/keiranmaskell/Desktop/3spp_defmut_code"
 
+#define working direcory
+if(readline("Is this your first time running this? (y/n)")=='y'){
+  wd <- readline("Specify working directory filepath")
+  setwd(wd)
+}else{
+  message("Using default working directory")
+  setwd(default_wd)
+}
 
-## list_of_packages <- c("openxlsx",
-##                       "matrixStats",
-##                       "jsonlite")
-
-## loadallpackages <- function(packagelist) {
-##     loaded_yn <- lapply(packagelist, require,
-##      character.only = TRUE, quietly = TRUE)
-##     return(loaded_yn)
-## }
-
-## loadallpackages(list_of_packages)
-library(matrixStats)
-library("beepr")
-
+#audio message to play upon error
 sorry_dave <- function(){
-   system("afplay /Users/keiranmaskell/Dropbox/tripartite-symbiosis/sorry_dave.mp3", 
+   system("afplay ./resources/sorry_dave.mp3
+", 
    intern=FALSE)  
 }
 
+#the list of R packages needed for this project
+list_of_packages <- c("matrixStats",
+                       "beepr","splines2")
+
+#check to see if packages are installed, if not, install them
+lapply(list_of_packages, function(pkg) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg, dependencies = TRUE)
+    library(pkg, character.only = TRUE)
+  }
+})
+
+#check to see if packages successfully loaded (and installed)
+#and return the names of any packages that failed to load
+loaded_packages <- lapply(list_of_packages, require,
+      character.only = TRUE, quietly = TRUE)
+
+if (all(unlist(loaded_packages))) {
+  message("Packages successfully loaded")
+} else {
+  message(sprintf("The following failed to load: %s",list_of_packages[loaded_packages==FALSE]))
+  sorry_dave()
+  stop("One or more packages failed to load")
+}
+
+
+#source all the R files needed for this project
 source('population_function.R')
 source('base_prms.R')
 source('meta_functions.R')
 source('plot_code.R')
  
+#initialize a population
 make.pop <- function(prms){
   pop.init <- array(rep(prms$init.pop.size, each=prms$npatch), dim = c(prms$npatch, 3))
   colnames(pop.init) <- c('f', 'm', 'r')
